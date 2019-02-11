bbecc9e512974d6a8e9784b00df6b1866e86134d
BartdeJong <bart.dejong375@gmail.com>
2019-02-11 14:55:28 +0100

    Started with Die and Board

---
+++ webapplication/src/components/Die.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="die">
+      {{char}}
+  </div>
+</template>
+
+<style lang="scss">
+.die {
+  width: 50px;
+  height: 50px;
+  border: 5px solid lightslategray;
+  text-align: center;
+  padding: 5px;
+  margin: 5px;
+}
+</style>
+
+<script>
+  export default {
+    name: "Die",
+    data() {
+      return {
+        char: "a",
+      };
+    },
+  }
+</script>
